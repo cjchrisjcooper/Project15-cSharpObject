ebbc9ac3aa5d9ff88dbfb579a2189dd7f36e536f
Chris Cooper <bloodwolf444@gmail.com>
2022-11-08 19:26:58 -0700

    Added MorseCodeTranslater class to project

---
+++ Project1-cSharpObject/MorseCodeTranslater.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_cSharpObject
+{
+    public static class MorseCodeTranslater
+    {
+        private static Dictionary<char, string> _textToMorse = new Dictionary<char, string>
+        {
+            {' ', "/"},
+            {'A', ".-"},
+            {'B', "-..."},
+            {'C', "-.-."},
+            {'D', "-.."},
+            {'E', "."},
+            {'F', "..-."},
+            {'G', "--."},
+            {'H', "...."},
+            {'I', ".."},
+            {'J', ".---"},
+            {'K', "-.-"},
+            {'L', ".-.."},
+            {'M', "--"},
+            {'N', "-."},
+            {'O', "---"},
+            {'P', ".--."},
+            {'Q', "--.-"},
+            {'R', ".-."},
+            {'S', "..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'W', ".--"},
+            {'X', "-..-"},
+            {'Y', "-.--"},
+            {'Z', "--.."},
+            {'1', ".----"},
+            {'2', "..---"},
+            {'3', "...--"},
+            {'4', "....-"},
+            {'5', "....."},
+            {'6', "-...."},
+            {'7', "--..."},
+            {'8', "---.."},
+            {'9', "----."},
+            {'0', "-----"},
+            {',', "--..--"},
+            {'.', ".-.-.-"},
+            {'?', "..--.."},
+            {';', "-.-.-."},
+            {':', "---..."},
+            {'/', "-..-."},
+            {'-', "-....-"},
+            {'\'', ".----."},
+            {'"', ".-..-."},
+            {'(', "-.--."},
+            {')', "-.--.-"},
+            {'=', "-...-"},
+            {'+', ".-.-."},
+            {'@', ".--.-."},
+            {'!', "-.-.--"},
+            {'Á', ".--.-"},
+            {'É', "..-.."},
+            {'Ö', "---."},
+            {'Ä', ".-.-"},
+            {'Ñ', "--.--"},
+            {'Ü', "..--"}
+        };
+
+
+        public static string TextToMorse(string phrase)
+        {
+            
+            List<string> output = new List<string>(phrase.Length);
+
+            foreach(char character in phrase.ToUpper())
+            {
+                try
+                {
+                    string morseCode = _textToMorse[character];
+                    output.Add(morseCode);
+                }
+                catch(KeyNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+              
+            }
+
+            return string.Join(" ", output);
+        }
+    }
+}
